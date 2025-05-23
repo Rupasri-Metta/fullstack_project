c0601cde24cda90515d84bd52192652a32854dea
Rupasri-Metta <mettarupasri11@gmail.com>
2025-05-23 21:51:36 +0530

    Initial commit with tutorial-based LMS project

---
+++ client/src/pages/student/CoursesList.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+
+const CoursesList = () => {
+  const {navigate} = useContext(AppContext)
+  return (
+       <>
+         <div>
+          <div>
+            <h1>Course List</h1>
+            <p className='text-gray-500'>
+              <span className='text-blue-600 cursor-pointer' onClick={()=>navigate('/')}>Home </span> / <span>Course List</span></p>
+            </div>
+         </div>
+       </> 
+  )
+}
+
+export default CoursesList
